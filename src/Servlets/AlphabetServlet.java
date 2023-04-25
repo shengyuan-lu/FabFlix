f9a47f38f23e6934b49accb8b144f41fddce694e
Shengyuan Lu <shengyuan.lu.developer@gmail.com>
2023-04-25 00:20:25 -0700

    Update AlphabetServlet.java

--- src/Servlets/AlphabetServlet.java
+++ src/Servlets/AlphabetServlet.java
@@ -1,6 +1,5 @@
 package Servlets;
 
-import Helpers.DatabaseHandler;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import jakarta.servlet.ServletConfig;
@@ -8,14 +7,8 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.List;
 
 @WebServlet(name = "AlphabetServlet", urlPatterns = "/api/alphabet")
 public class AlphabetServlet extends HttpServlet {
