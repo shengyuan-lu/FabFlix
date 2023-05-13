099590cd97c1aab13b7ea8073600da105720cfb1
Tony Liu <tonylappdev@gmail.com>
2023-05-13 16:41:36 -0700

    set up xml parsing

---
+++ xmlParser/MainParser.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+
+import org.xml.sax.helpers.DefaultHandler;
+
+public class MainParser extends DefaultHandler {
+
+    public static void main(String[] args) {
+        StarSAXParser starParser = new StarSAXParser();
+        starParser.runExample();
+    }
+
+}
