daca289815ba2a05cc899415b3309fafaf334fbb
Shengyuan Lu <shengyuan.lu.developer@gmail.com>
2023-05-13 19:07:28 -0700

    update movie and star model

--- xmlParser/models/Movie.java
+++ xmlParser/models/Movie.java
@@ -3,13 +3,14 @@ package models;
 import java.util.ArrayList;
 
 public class Movie {
-    private String id;
-    private String title;
-    private int year;
-    private String director;
-    private ArrayList<String> genres;
+    private String id; // id in table (can't be null)
+    private String title; // title in table (can't be null)
+    private int year; // year in table, int (can't be null)
+    private String director; // director in table (can't be null)
+    private double price = 7.99; // price in table (can't be null), amount doesn't matter so fixed for now
+    private ArrayList<String> genres; // 1 movie can have multiple genres
 
-    public Movie(String id, String title, Integer year, String director, ArrayList<String> genres) {
+    public Movie(String id, String title, int year, String director, ArrayList<String> genres) {
         this.id = id;
         this.title = title;
         this.year = year;
@@ -25,12 +26,16 @@ public class Movie {
         return title;
     }
 
-    public Integer getYear() {
+    public int getYear() {
         return year;
     }
 
     public String getDirector() {
         return director;
+    }
+
+    public double getPrice() {
+        return price;
     }
 
     public ArrayList<String> getGenres() {
--- xmlParser/models/Star.java
+++ xmlParser/models/Star.java
@@ -1,5 +1,19 @@
 package models;
 
 public class Star {
-}
+    private String name; // name in table (can't be null)
+    private int birthYear; // birthYear in table (can be null)
 
+    public void Star(String name, int birthYear) {
+        this.name = name;
+        this.birthYear = birthYear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+}
