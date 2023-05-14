dbb57b03743d1447fcf838a8cdc6a61b336441a3
Shengyuan Lu <shengyuan.lu.developer@gmail.com>
2023-05-13 19:09:14 -0700

    Update Star.java

--- xmlParser/models/Star.java
+++ xmlParser/models/Star.java
@@ -4,7 +4,7 @@ public class Star {
     private String name; // name in table (can't be null)
     private int birthYear; // birthYear in table (can be null)
 
-    public void Star(String name, int birthYear) {
+    public Star(String name, int birthYear) {
         this.name = name;
         this.birthYear = birthYear;
     }
