7fa846b00ba9163e0ebb5992e9b741eae9a5bbbb
Tony Liu <tonylappdev@gmail.com>
2023-05-24 15:35:47 -0700

    update api connection for android login

--- AndroidContent/app/src/main/java/edu/uci/ics/fabflixmobile/ui/login/LoginActivity.java
+++ AndroidContent/app/src/main/java/edu/uci/ics/fabflixmobile/ui/login/LoginActivity.java
@@ -28,9 +28,9 @@ public class LoginActivity extends AppCompatActivity {
       To connect to your machine, you need to use the below IP address
      */
     private final String host = "10.0.2.2";
-    private final String port = "8080";
-    private final String domain = "cs122b_project2_login_cart_example_war";
-    private final String baseURL = "http://" + host + ":" + port + "/" + domain;
+    private final String port = "8443";
+    private final String domain = "cs122b_fabflix_war";
+    private final String baseURL = "https://" + host + ":" + port + "/" + domain;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
