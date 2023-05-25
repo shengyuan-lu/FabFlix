7524cf1b84c6b0c2d547d0381d7a463776cc9e49
Shengyuan Lu <shengyuan.lu.developer@gmail.com>
2023-05-25 03:29:36 -0700

    Update MovieListServlet.java

--- src/Servlets/MovieListServlet.java
+++ src/Servlets/MovieListServlet.java
@@ -1,21 +1,21 @@
 package Servlets;
 
+import Helpers.DatabaseHandler;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.List;
-import Helpers.DatabaseHandler;
 
 @WebServlet(name = "MovieListServlet", urlPatterns = "/api/movies")
 public class MovieListServlet extends HttpServlet {
