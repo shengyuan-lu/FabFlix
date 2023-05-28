1299f780508631beeb77d81c98f47c62544c330d
Shengyuan Lu <shengyuan.lu.developer@gmail.com>
2023-05-28 15:43:33 -0700

    Update Constants.java

--- AndroidContent/app/src/main/java/edu/uci/ics/fabflixmobile/data/Constants.java
+++ AndroidContent/app/src/main/java/edu/uci/ics/fabflixmobile/data/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
       In Android, localhost is the address of the device or the emulator.
       To connect to your machine, you need to use the below IP address
      */
-    public static final String host = "10.0.2.2";
+    public static final String host = "ec2-54-215-189-142.us-west-1.compute.amazonaws.com";
     public static final String port = "8443";
     public static final String domain = "cs122b_fabflix_war";
     public static final String baseURL = "https://" + host + ":" + port + "/" + domain;
